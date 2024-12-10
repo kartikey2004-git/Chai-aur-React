b6e30ba1e0c32161edea5a80d2a259d3e1ac3fea
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-12-10 23:33:07 +0530

    redux toolkit config

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,42 @@
+import { useState , useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authslice'
 import './App.css'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading , setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-    <h1> A Blog app with appwrite </h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          { /* TODO : <Outlet /> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
+
+// In React, dispatch is a function that sends an action to the Redux store, which triggers a state change in the application
+
