3e74fbf0d227798f81fa835577d95477d559e874
kartikey2004-git <kartikeybhatnagar247@gmail.com>
2025-01-21 00:40:46 +0530

    remove blog post from current directory

--- 12MegaBlog/src/components/container/Container.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react/prop-types */
-
-function Container({ children }) {
-  return <div className="w-screen max-w-screen-2xl mx-auto px-4 ">{children}</div>;
-}
-
-export default Container;
